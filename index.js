964610382c4457675aa81a253df31be1a6bed307
manoj-0 <manojjangid2025@gmail.com>
2021-10-22 19:06:45 +0530

    nodelearn

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const app = express();
 //for parsing the save to json data middle ware
 app.use(express.json());
 
-app.get("/", function (req, res) {
-  console.log(req.query.name);
+const fun_get = (req, res) => {
+  console.log(req.query);
   res.send("Hello World from get");
-  // console.log("after");
-});
-app.post("/", () => fu_get(req, res));
+};
+app.get("/", fun_get);
+// app.post("/", () => fu_get(req, res));
 app.delete("/:id", function (req, res) {
   console.log(req.params.id);
   return res.send("Hello World from delete");
