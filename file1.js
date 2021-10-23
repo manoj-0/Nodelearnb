7df9a8e6b9af77ec8e1cee2a4d5c5f7667255e82
manoj-0 <manojjangid2025@gmail.com>
2021-10-23 09:50:28 +0530

    fun

---
+++ file1.js
@@ -0,0 +1,13 @@
+// const name = "manoj";
+// const address = "jaipur";
+class File1 {
+  static name = "manoj";
+  static address = "jaipur";
+
+  static fun = () => {
+    console.log("function called");
+  };
+}
+
+// module.exports = { name, address };
+module.exports = File1;
---
+++ file2.js
@@ -0,0 +1,5 @@
+// const { name, address } = require("./file1");
+const File1 = require("./file1");
+// console.log(name);
+console.log(File1);
+File1.fun();
